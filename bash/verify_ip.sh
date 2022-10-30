e697b0f79301a255f32784271af8375045e2e8cf
bradel69 <bradel69@users.noreply.github.com>
2022-10-30 20:07:52 +0000

    Update verify_ip.sh

--- bash/verify_ip.sh
+++ bash/verify_ip.sh
@@ -9,7 +9,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
 EXTERNAL_ATUAL_IP=$(curl -s http://whatismyip.akamai.com/)
 # verify last ip
 OLD_IP=$(cat /root/scripts/old_ip.txt)
-# cretae file to send by email
+# create file to send by email
 # write date
 date > /root/scripts/email_external_ip.txt
 # write external ip
